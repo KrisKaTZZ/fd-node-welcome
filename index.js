40682341a2526cfacce1cef83b266a6162253e1c
Kirusha <kriskat58@gmail.com>
2022-11-08 14:18:24 +0200

    One

---
+++ index.js
@@ -0,0 +1,15 @@
+const fs = require('fs').promises;
+
+const promise = fs.readFile('./userData.json', 'utf-8')
+                    .then(promiseValue => {
+                        const obj = JSON.parse(promiseValue);
+                        fs.writeFile('text.txt', `Hello, ${obj.firstName} ${obj.lastName}`)
+                    })
+
+/*
+Прочитати дані з userData.json
+На основі розпарсених даних вивести в файл text.txt рядок тексту типу
+"Hello, {firstName} {lastName}"
+*/
+
+                   
